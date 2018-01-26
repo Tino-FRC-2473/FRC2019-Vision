a916db3ded04bd0c15c6bc8fabd3a3043048e178
Shruthi Jaganathan <shruthijaganathan@gmail.com>
2018-01-25 21:15:44 -0800

    Fix syntax errors and add checks for num of rects
    Able to find parts of rectangles, distance constant is inaccurate

--- cv.py
+++ cv.py
@@ -27,7 +27,7 @@ class VisionTargetDetector:
             return self.DIST_CONSTANT / length
         return -1
 
-    def getClosestRects(r1, r2, r3):
+    def getClosestRects(self, r1, r2, r3):
         dist1 = math.hypot(r1.x-r2.x, r1.y - r2.y)
         dist2 = math.hypot(r1.x-r3.x, r1.y - r3.y)
         dist3 = math.hypot(r2.x-r3.x, r2.y - r3.y)
@@ -44,7 +44,7 @@ class VisionTargetDetector:
         pinDistToCenter = pinX - self.SCREEN_WIDTH / 2
         return math.atan(pinDistToCenter / self.ANGLE_CONST)
 
-    def cakcPinPosition(self, x1, y1, x2, y2, x3, y3, x4, y4):
+    def calcPinPosition(self, x1, y1, x2, y2, x3, y3, x4, y4):
         x = (x1 + x2 + x3 + x4) / 4.0
         y = (y1 + y2 + y3 + y4) / 4.0
         return (int(x), int(y))
@@ -70,21 +70,31 @@ class VisionTargetDetector:
             area = cv2.contourArea(c)
             maxContours.append(Rectangle(x, y, w, h, area))
 
-        rect1, rect2, rect3 = maxContours
+        if (len(maxContours) < 2) :
+            return -1, -1
+        rect1 = maxContours[0]
+        rect2 = maxContours[1]
+        rect3 = None
+        if len(maxContours) < 3 :
+            rect3 = Rectangle(0, 0, 0, 0, 0)
+        else :
+            rect3 = maxContours[2]
         oneRect = False
 
         if rect3.area != 0:
             rect1, rect2 = self.getClosestRects(rect1, rect2, rect3)
 
+        self.pinX = 0
+        self.pinY = 0
         #draw rectangles on two biggest green part found, draws green rectangles
         if(rect1.area > 0):
-            cv2.rectangle(frame, (rect1.x, rect1.y, (rect1.x + rect1.width, rect1.y + rect1.height), (0, 255, 0), thickness=3)
+            cv2.rectangle(frame, (rect1.x, rect1.y), (rect1.x + rect1.width, rect1.y + rect1.height), (0, 255, 0), thickness=3)
             #sets threshold for second rectangle length
             if(rect2.height > 0.3 * rect1.width and rect2.width > 0):
-                cv2.rectangle(frame, (rect2.x, rect2.y, (rect2.x + rect2.width, rect2.y + rect2.height), (0,255,0), thickness=3)
+                cv2.rectangle(frame, (rect2.x, rect2.y), (rect2.x + rect2.width, rect2.y + rect2.height), (0,255,0), thickness=3)
 
                 #finds the approximate position of the pin and draws a blue circle in that position
-                self.pinX, self.pinY = self.calcPinPosition(rect1.x, rect1.y, (rect1.x + rect1.width, rect1.y + rect1.height, rect2.x, rect2.y, (rect2.x + rect2.width, rect2.y + rect2.height)
+                self.pinX, self.pinY = self.calcPinPosition(rect1.x, rect1.y, rect1.x + rect1.width, rect1.y + rect1.height, rect2.x, rect2.y, rect2.x + rect2.width, rect2.y + rect2.height)
                 cv2.circle(frame, (self.pinX, self.pinY), 1, (255, 0, 0), thickness=5)
 
             #one rectangle case, when second rectagle is too small/nonexistent
@@ -98,7 +108,8 @@ class VisionTargetDetector:
 
         angle = self.calcAngleDeg(self.pinX)
         distance = self.calcDist((rect1.height + rect2.height) / 2.0)
-        if (oneRect) distance = self.calcDist(rect1.height)
+        if (oneRect):
+             distance = self.calcDist(rect1.height)
 
         cv2.putText(frame, "ANG: " + str(angle), (0, 50), cv2.FONT_HERSHEY_DUPLEX, 2, 255)
         cv2.putText(frame, "DIST: " + str(distance), (0, 120), cv2.FONT_HERSHEY_DUPLEX, 2, 255)
