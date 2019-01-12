a0189dadbff7f8efb31a084d1e1f2057896ceaee
Michelle <michellepan8@gmail.com>
2019-01-12 10:10:32 -0800

    Clean up code
    Added Rectangle class to store contour properties and removed
    unused code.

--- cv.py
+++ cv.py
@@ -1,39 +1,25 @@
+# add imports
 import math
 import cv2
 import numpy as np
 import os
 
-#finds and returns the angle to Deep Space Vision Targets
+# finds angle between robot's heading and the perpendicular to the targets
 class VisionTargetDetector:
 
-    #initializes the variables used in this class
+    # initilaze variables
     def __init__(self):
-        #rectSize = 10; # set the rext size
-        self.DIST_CONSTANT = 3300 # need to test this
-        #For angle
-        #calc the constant
-        self.FIELD_OF_VIEW_RAD = 70.42 * math.pi / 180.0;
-        #note: total not just half of screen
-        #edges always distorted
-        self.SCREEN_WIDTH = 0;
-        self.ANGLE_CONST = 0;
 
-        #calc angle
         self.angle = -1
-        self.SCREEN_HEIGHT = 0;
-        self.pinX = 0;
-        self.pinY = 0;
-        self.pinDistToCenter = 0;
-
-        self.TOP = False
-        self.BOTTOM = True
-        # angle;
 
         os.system('sudo sh camerasettings.sh')
-        self.camera = cv2.VideoCapture(0)
-        # camera = cv2.VideoCapture(1)
+        self.camera = cv2.VideoCapture(1)
         _, frame = self.camera.read()
+
         self.SCREEN_HEIGHT, self.SCREEN_WIDTH = frame.shape[:2]
+
+        self.FIELD_OF_VIEW_RAD = 70.42 * math.pi / 180.0
+        self.DIST_CONSTANT = 3300 # need to test this
         self.ANGLE_CONST = (self.SCREEN_WIDTH / 2.0) / math.tan(self.FIELD_OF_VIEW_RAD / 2.0)
 
     def calcDist(self, length):
@@ -42,149 +28,94 @@ class VisionTargetDetector:
         return -1
 
     def getClosestRects(r1, r2, r3):
-        dist1 = math.hypot(r1[1]-r2[1], r1[2]-r2[2])
-        dist2 = math.hypot(r1[1]-r3[1], r1[2]-r3[2])
-        dist3 = math.hypot(r2[1]-r3[1], r2[2]-r3[2])
+        dist1 = math.hypot(r1.x-r2.x, r1.y - r2.y)
+        dist2 = math.hypot(r1.x-r3.x, r1.y - r3.y)
+        dist3 = math.hypot(r2.x-r3.x, r2.y - r3.y)
         maximum = max(dist1, dist2, dist3)
 
         if maximum == dist1: return r1, r2
         elif maximum == dist2: return r1, r3
         else: return r2, r3
 
-    def getRectPos(self, y1, h1, y3, h2):
-        rectOnBottom = False
-        if(y1 < y3):
-            
-            if( ((y1 + h1) - (y3 + h2)) < (y3 - y1) ):
-                #deltaH = (y1 + h1) - (y3 + h2)
-                rectOnBottom = True
-            else:
-                #deltaH = y3 - y1
-                rectOnBottom = False
-
-        else:
-            #deltaH = y1 - y3
-            if( ((y3 + h2) - (y1 + h1)) < (y1 - y3) ):
-                #deltaH = (y3 + h2) - (y1 + h1)
-                rectOnBottom = True
-            else:
-                #deltaH = y1 - y3
-                rectOnBottom = False
-
-        return rectOnBottom
-
-    #calculates the angle in degrees
-    #we need to turn to be centered with the back of the board
     def calcAngleDeg(self, pinX):
         return self.calcAngleRad(pinX) * 180.0 / math.pi
 
-    #calculates the angle in radians
-    #we need to turn to be centered with the back of the board
     def calcAngleRad(self, pinX):
-        pinDistToCenter = self.calcPinDist(pinX)
-        #returns it in radians
+        pinDistToCenter = pinX - self.SCREEN_WIDTH / 2
         return math.atan(pinDistToCenter / self.ANGLE_CONST)
 
-    #helper method to calculate the horizontal distance
-    #between the center of the screen and the peg in pixels
-    def calcPinDist(self, pinX):
-        #if the peg is on the right side of the screen, POSITIVE
-        #peg on left side of screen, NEGATIVE
-        return (pinX - self.SCREEN_WIDTH / 2);
-        #return math.fabs(pinX - SCREEN_WIDTH / 2);
-
-    #calculates the approximate position of the peg on the screen
-    def pinPosition(self, x1, y1, x2, y2, x3, y3, x4, y4):
-        x = (x1 + x2 + x3 + x4) / 4.0;
-        y = (y1 + y2 + y3 + y4) / 4.0;
+    def cakcPinPosition(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        x = (x1 + x2 + x3 + x4) / 4.0
+        y = (y1 + y2 + y3 + y4) / 4.0
         return (int(x), int(y))
 
-    #returns the calculated angle
     def runCV(self):
-        #gets a frame/picture of what the camera is getting
         _, frame = self.camera.read()
-
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        low_green = np.array([60, 90.0, 50.0]) 
+        low_green = np.array([60, 90.0, 50.0])
         high_green = np.array([87, 255, 229.0])
 
-        #makes mask
         mask = cv2.inRange(hsv, low_green, high_green)
+        _, contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        #shows mask
-        #cv2.imshow("Mask", mask)
+        def sort_by_area(c):
+            return cv2.contourArea(c)
 
-        #find contours based on mask
-        _, contour,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours.sort(key=sort_by_area, reverse=True)
+        maxContours = []
 
-        #initializes rectangle variables with a dummy value
-        max_rect = (0, 0, 0, 0, 0)
-        smax_rect = (0, 0, 0, 0, 0)
-        tmax_rect = (0, 0, 0, 0, 0)
+        for c in contours[:3]:
+            x, y, w, h = cv2.boundingRect(c)
+            area = cv2.contourArea(c)
+            maxContours.append(Rectangle(x, y, w, h, area))
 
-        #finds the three biggest rectangles
-        for cnt in contour:
-            x,y,w,h = cv2.boundingRect(cnt)
-
-            area = cv2.contourArea(cnt)
-
-            if(area >= max_rect[0]):
-                tmax_rect = smax_rect
-                smax_rect = max_rect
-                max_rect = (area, x, y, w, h)
-            elif(area >= smax_rect[0]):
-                tmax_rect = smax_rect
-                smax_rect = (area, x, y, w, h)
-            elif(area >= tmax_rect[0]):
-                tmax_rect = (area, x, y, w, h)
-
-        
+        rect1, rect2, rect3 = maxContours
         oneRect = False
-        
-        #find two closest rectangles
-        if tmax_rect[0] != 0:
-            max_rect, smax_rect = self.getClosestRects(max_rect, smax_rect, tmax_rect)
+
+        if rect3.area != 0:
+            rect1, rect2 = self.getClosestRects(rect1, rect2, rect3)
 
         #draw rectangles on two biggest green part found, draws green rectangles
-        if(max_rect[0] > 0):
-            cv2.rectangle(frame,(max_rect[1],max_rect[2]),(max_rect[1]+max_rect[3],max_rect[2]+max_rect[4]),(0,255,0),thickness=3)
+        if(rect1.area > 0):
+            cv2.rectangle(frame, (rect1.x, rect1.y, (rect1.x + rect1.width, rect1.y + rect1.height), (0, 255, 0), thickness=3)
             #sets threshold for second rectangle length
-            if(smax_rect[4] > 0.3 * max_rect[3] and smax_rect[3] > 0):
-                cv2.rectangle(frame,(smax_rect[1],smax_rect[2]),(smax_rect[1]+smax_rect[3],smax_rect[2]+smax_rect[4]),(0,0,255),thickness=3)
+            if(rect2.height > 0.3 * rect1.width and rect2.width > 0):
+                cv2.rectangle(frame, (rect2.x, rect2.y, (rect2.x + rect2.width, rect2.y + rect2.height), (0,255,0), thickness=3)
 
                 #finds the approximate position of the pin and draws a blue circle in that position
-                self.pinX, self.pinY = self.pinPosition(max_rect[1], max_rect[2], max_rect[1]+max_rect[3], max_rect[2]+max_rect[4], smax_rect[1], smax_rect[2], smax_rect[1]+smax_rect[3], smax_rect[2]+smax_rect[4])
+                self.pinX, self.pinY = self.calcPinPosition(rect1.x, rect1.y, (rect1.x + rect1.width, rect1.y + rect1.height, rect2.x, rect2.y, (rect2.x + rect2.width, rect2.y + rect2.height)
                 cv2.circle(frame, (self.pinX, self.pinY), 1, (255, 0, 0), thickness=5)
 
             #one rectangle case, when second rectagle is too small/nonexistent
             else:
                 oneRect = True
-                if(max_rect[1] + max_rect[3]/2.0 > self.SCREEN_WIDTH / 2.0):
-                    self.pinX = max_rect[1] + 5.125/5*max_rect[4]
+                if(rect1.x + rect1.width/2.0 > self.SCREEN_WIDTH / 2.0):
+                    self.pinX = rect1.x + 5.125/5*rect1.height
                 else:
-                    self.pinX = max_rect[1] - 3.125/5*max_rect[4]
+                    self.pinX = rect1.x - 3.125/5*rect1.height
 
 
         angle = self.calcAngleDeg(self.pinX)
-        distance = self.calcDist((max_rect[4] + smax_rect[4]) / 2.0)
-        if (oneRect) distance = self.calcDist(max_rect[4])
+        distance = self.calcDist((rect1.height + rect2.height) / 2.0)
+        if (oneRect) distance = self.calcDist(rect1.height)
 
-        #displays data on the screen such as the angle
-        cv2.putText(frame, "ANG: " + str(angle), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
-        cv2.putText(frame, "DIST: " + str(distance), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
-        #displays data on the console such as the angle
-        #print "Angle: " + str(angle)
-        #print "--------------------"
+        cv2.putText(frame, "ANG: " + str(angle), (0, 50), cv2.FONT_HERSHEY_DUPLEX, 2, 255)
+        cv2.putText(frame, "DIST: " + str(distance), (0, 120), cv2.FONT_HERSHEY_DUPLEX, 2, 255)
 
-        #displays the screen showing the contours
-        #cv2.imshow("Contours", mask)
+        cv2.imshow("Contours", mask)
+        cv2.imshow("Frame", frame)
 
-        #displays the frame, which we use to visualize rectangles with the drawing
-        #cv2.imshow("Frame", frame)
-
-        #adding lag time so we can look at the data more carefully
         cv2.waitKey(3)
 
-        #returns and angle we calculated
         return angle, distance
+
+# holds rectangle properties
+class Rectangle:
+
+    def __init__(self, x, y, width, height, area):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.area = area
